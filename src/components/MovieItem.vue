e8929a48397ab51aef8e389b973674c07cd3e1b0
Andrearusso94 <andrea.r.4845@gmail.com>
2022-11-16 17:33:34 +0100

    end Flag mileston 2

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -14,9 +14,20 @@ export default {
 
             <h5>{{ movie.title }}</h5>
             <h4 class="text-secondary">{{ movie.original_title }}</h4>
-            <p class="text-secondary">{{ movie.original_language }}</p>
+            <img class="img-fluid rounded-circle" v-if="movie.original_language === 'it'" src="../assets/img/ita.png"
+                alt="">
+            <img class="img-fluid" v-else-if="movie.original_language === 'en'" src="../assets/img/eng.webp" alt="">
+            <img class="img-fluid" v-else-if="movie.original_language === 'fr'" src="../assets/img/france.png" alt="">
+            <p v-else>{{ movie.original_language }}</p>
             <p>{{ movie.vote_average }}</p>
 
         </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+img {
+    width: 25px;
+    height: 25px;
+}
+</style>
